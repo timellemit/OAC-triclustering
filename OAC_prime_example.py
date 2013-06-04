1042a6561a312359912c22a5fa8d7cca0efb1ecf
Юрий Кашницкий <yurec@packardbell.(none)>
2013-06-05 00:14:02 +0400

    with GUI

--- OAC_prime_example.py
+++
@@ -1,8 +0,0 @@
-import OAC_prime
-
-context = OAC_prime.Context('input/context_gen_1x444x5193.txt')
-min_density = 0.2
-
-# returns a dictionary of triclusters {hash_key : [user, tag, resource]}
-Triclusters = context.triclusters(min_density)
-print 'Triclusters: ', Triclusters.values()
